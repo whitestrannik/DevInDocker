fd393cd4d1c770b61caa158d2388c4921804e229
Mikle <whitestrannik@outlook.com>
2020-05-28 23:58:37 +0300

    Added Powershell to core image. Changed base image to 18.04

--- BaseDevImage/base_install.sh
+++ BaseDevImage/base_install.sh
@@ -4,6 +4,22 @@ set -e
 echo "Install common tools and libs"
 apt-get update 
 apt-get install -y nano mc wget net-tools locales bzip2 python-numpy
+
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+
+echo "Install Powershell core"
+# Register the Microsoft repository GPG keys
+dpkg -i packages-microsoft-prod.deb
+
+# Update the list of products
+apt-get update
+
+# Enable the "universe" repositories
+#add-apt-repository universe
+
+# Install PowerShell
+apt-get install -y powershell
+
 apt-get clean -y
 
 echo "generate locales en_US.UTF-8"
