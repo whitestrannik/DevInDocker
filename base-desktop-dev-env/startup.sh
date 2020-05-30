ba497adf1a899cedd9d90196451a9e0564ccd002
Mikle <whitestrannik@outlook.com>
2020-05-30 14:30:54 +0300

    Vnc is work. Cleaning

--- base-desktop-dev-env/startup.sh
+++ base-desktop-dev-env/startup.sh
@@ -1,78 +1,53 @@
 #!/bin/bash
 set -e
 
- apt-get install -y ttf-wqy-zenhei
 ## correct forwarding of shutdown signal
-# cleanup () {
-#     kill -s SIGTERM $!
-#     exit 0
-# }
-# trap cleanup SIGINT SIGTERM
+cleanup () {
+    kill -s SIGTERM $!
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
+
+
 
 ## write correct window size to chrome properties
-./$INST_DIR/chrome-init.sh
+VNC_RES_W=${VNC_RESOLUTION%x*}
+VNC_RES_H=${VNC_RESOLUTION#*x}
 
-## resolve_vnc_connection
-VNC_IP=$(hostname -i)
+echo -e "\n------------------ update chromium-browser.init ------------------"
+echo -e "\n... set window size $VNC_RES_W x $VNC_RES_H as chrome window size!\n"
+
+echo "CHROMIUM_FLAGS='--no-sandbox --disable-gpu --user-data-dir --window-size=$VNC_RES_W,$VNC_RES_H --window-position=0,0'" > $ROOT_DIR/.chromium-browser.init
+
+
 
 ## change vnc password
 echo -e "\n------------------ change VNC password  ------------------"
-# first entry is control, second is view (if only one is valid for both)
 mkdir -p "$ROOT_DIR/.vnc"
 PASSWD_PATH="$ROOT_DIR/.vnc/passwd"
 
-# if [[ -f $PASSWD_PATH ]]; then
-#     echo -e "\n---------  purging existing VNC password settings  ---------"
-#     rm -f $PASSWD_PATH
-# fi
+if [[ -f $PASSWD_PATH ]]; then
+    echo -e "\n---------  purging existing VNC password settings  ---------"
+    rm -f $PASSWD_PATH
+fi
 
-# if [[ $VNC_VIEW_ONLY == "true" ]]; then
-#     echo "start VNC server in VIEW ONLY mode!"
-#     #create random pw to prevent access
-#     echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
-# fi
 echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
-# echo "$VNC_PW"
-# cat $PASSWD_PATH
 chmod 600 $PASSWD_PATH
 
 
-## start vncserver and noVNC webclient
-# echo -e "\n------------------ start noVNC  ----------------------------"
-# if [[ $DEBUG == true ]]; then echo "$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT"; fi
-# $NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &> $STARTUPDIR/no_vnc_startup.log &
-# PID_SUB=$!
-
-# echo -e "\n------------------ start VNC server ------------------------"
-# echo "remove old vnc locks to be a reattachable container"
-vncserver -kill $DISPLAY &> ./$INST_DIR/vnc_startup.log \
-    || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> ./$INST_DIR/vnc_startup.log \
+## start vnc server
+echo -e "\n------------------ start VNC server ------------------------"
+vncserver -kill $DISPLAY &> $ROOT_DIR/$INST_DIR/vnc_startup.log \
+    || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $ROOT_DIR/$INST_DIR/vnc_startup.log \
     || echo "no locks present"
 
 echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
-if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
 vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION
 echo -e "start window manager\n..."
-./wm_startup.sh &> ./$INST_DIR/wm_startup.log
+./wm_startup.sh &> $ROOT_DIR/$INST_DIR/wm_startup.log
 
-## log connect options
+VNC_IP=$(hostname -i)
 echo -e "\n\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
-echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
 
-tail -f ./$INST_DIR/*.log $ROOT_DIR/.vnc/*$DISPLAY.log
-
-# if [[ $DEBUG == true ]] || [[ $1 =~ -t|--tail-log ]]; then
-#     echo -e "\n------------------ $HOME/.vnc/*$DISPLAY.log ------------------"
-#     # if option `-t` or `--tail-log` block the execution and tail the VNC log
-#     tail -f $STARTUPDIR/*.log $HOME/.vnc/*$DISPLAY.log
-# fi
-
-# if [ -z "$1" ] || [[ $1 =~ -w|--wait ]]; then
-#     wait $PID_SUB
-# else
-#     # unknown option ==> call command
-#     echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
-#     echo "Executing command: '$@'"
-#     exec "$@"
-# fi
+tail -f $ROOT_DIR/$INST_DIR/*.log $ROOT_DIR/.vnc/*$DISPLAY.log
--- base-desktop-dev-env/tigervnc_install.sh
+++ base-desktop-dev-env/tigervnc_install.sh
@@ -2,5 +2,6 @@
 set -e
 
 echo "Install TigerVNC server"
+apt-get install -y ttf-wqy-zenhei
 apt-get -y install python-numpy
 wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
